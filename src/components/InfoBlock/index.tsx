8a7361c0bbe00102984f1f75746074eefd49162f
Alexey Kohanovsky <awfj837@gmail.com>
2024-09-11 20:13:29 +0300

    add InfoBlock component

---
+++ src/components/InfoBlock/index.tsx
@@ -0,0 +1,25 @@
+import styles from './styles.module.css';
+
+import Bookmark from '@/components/Bookmark';
+import { Artwork } from '@/types/artworkTypes';
+
+type InfoBlockProps = {
+	artwork: Artwork;
+	fixed?: boolean;
+};
+
+export default function InfoBlock({ artwork, fixed = true }: InfoBlockProps) {
+	return (
+		<div
+			className={`${styles['info-block']} ${fixed ? styles.fixed : styles.floaty}`}
+		>
+			<div className={styles.info}>
+				<p className={styles.title}>{artwork.title}</p>
+				<p className={styles.artist}>{artwork.artist}</p>
+				<p className={styles['domain-type']}>{artwork.domainType}</p>
+			</div>
+
+			<Bookmark artwork={artwork} />
+		</div>
+	);
+}
