9580e4017d49254afdfd4f57c2aeb60a1ca4480f
Alexey Kohanovsky <awfj837@gmail.com>
2024-09-13 02:39:47 +0300

    add Card component

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,42 @@
+import { useNavigate } from 'react-router-dom';
+
+import styles from './styles.module.css';
+
+import NoImage from '@/assets/no-image.png';
+import InfoBlock from '@/components/InfoBlock';
+import { CardSize } from '@/types/app';
+import { Artwork } from '@/types/artwork';
+
+type CardProps = {
+	artwork: Artwork;
+	size?: CardSize;
+};
+
+export default function Card({ artwork, size = CardSize.Normal }: CardProps) {
+	const navigate = useNavigate();
+
+	const handleClick = () => {
+		navigate(`/detail-info/${artwork.id}`);
+	};
+
+	return (
+		<>
+			{artwork && (
+				<div
+					onClick={handleClick}
+					className={`${size === CardSize.Normal ? styles.normal : styles.small}`}
+				>
+					{artwork.image || artwork.thumbnail ? (
+						<img
+							src={size === CardSize.Normal ? artwork.image : artwork.thumbnail}
+							alt={artwork.title}
+						/>
+					) : (
+						<img src={NoImage} alt={artwork.title} />
+					)}
+					<InfoBlock artwork={artwork} fixed={size === CardSize.Small} />
+				</div>
+			)}
+		</>
+	);
+}
