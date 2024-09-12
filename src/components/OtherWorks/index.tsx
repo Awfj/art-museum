e9a128c3d838f69d690e3e685a21bc7c5ab2232f
Alexey Kohanovsky <awfj837@gmail.com>
2024-09-13 02:57:20 +0300

    add OtherWorks component

---
+++ src/components/OtherWorks/index.tsx
@@ -0,0 +1,29 @@
+import { useSelector } from 'react-redux';
+
+import CardsList from '@/components/CardsList';
+import ContentHeading from '@/components/ContentHeading';
+import { ARTWORKS_IN_GALLERY, ARTWORKS_TOTAL } from '@/constants/artworks';
+import { RootState } from '@/types/store';
+
+export default function OtherWorks() {
+	const favoritesCount = useSelector(
+		(state: RootState) => state.artworks.favorites
+	).length;
+	const artworks = useSelector((state: RootState) => state.artworks.artworks)
+		.slice(0, -favoritesCount)
+		.slice(ARTWORKS_IN_GALLERY, ARTWORKS_TOTAL);
+
+	if (artworks.length === 0) {
+		return null;
+	}
+
+	return (
+		<section>
+			<ContentHeading
+				heading={'Other works for you'}
+				description={'Here some more'}
+			/>
+			<CardsList artworks={artworks} />
+		</section>
+	);
+}
