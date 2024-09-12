bc8fa916af44675b603c23e28e84be763102f913
Alexey Kohanovsky <awfj837@gmail.com>
2024-09-13 02:33:02 +0300

    add Bookmark component

--- src/components/Bookmark/index.tsx
+++ src/components/Bookmark/index.tsx
@@ -1,14 +1,29 @@
+import { useDispatch } from 'react-redux';
+
 import styles from './styles.module.css';
 
 import BookmarkIcon from '@/assets/bookmark.svg';
+import { setFavorite } from '@/store/actions/artworks';
+import { Artwork } from '@/types/artwork';
 
 type BookmarkProps = {
 	floaty?: boolean;
+	artwork: Artwork;
 };
 
-export default function Bookmark({ floaty }: BookmarkProps) {
+export default function Bookmark({ artwork, floaty }: BookmarkProps) {
+	const dispatch = useDispatch();
+
+	function handleClick(e: React.MouseEvent<HTMLButtonElement>) {
+		e.stopPropagation();
+		dispatch(setFavorite({ artwork }));
+	}
+
 	return (
-		<button className={`${styles.bookmark} ${floaty ? styles.floaty : ''}`}>
+		<button
+			onClick={handleClick}
+			className={`${styles.bookmark} ${artwork.favorite ? styles.favorite : ''} ${floaty ? styles.floaty : ''}`}
+		>
 			<BookmarkIcon />
 		</button>
 	);
