a02a22651a7938705ffde0f213d92add9d4dd780
Alexey Kohanovsky <awfj837@gmail.com>
2024-09-13 15:58:35 +0300

    refactor OtherWorks component to display favorites when necessary

--- src/components/OtherWorks/index.tsx
+++ src/components/OtherWorks/index.tsx
@@ -6,14 +6,17 @@ import { ARTWORKS_IN_GALLERY, ARTWORKS_TOTAL } from '@/constants/artworks';
 import { RootState } from '@/types/store';
 
 export default function OtherWorks() {
-	const favoritesCount = useSelector(
+	const favorites = useSelector(
 		(state: RootState) => state.artworks.favorites
-	).length;
+	);
 	let artworks = useSelector((state: RootState) => state.artworks.artworks);
-	if (favoritesCount > 0) {
-		artworks = artworks.slice(0, -favoritesCount);
+	if (favorites.length > 0) {
+		artworks = artworks.slice(0, -favorites.length);
 	}
 	artworks = artworks.slice(ARTWORKS_IN_GALLERY, ARTWORKS_TOTAL);
+	if (artworks.length === 0) {
+		artworks = favorites;
+	}
 
 	if (artworks.length === 0) {
 		return null;
