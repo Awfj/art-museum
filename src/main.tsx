a7528c4f70068c4604a2637833d31bd237e855d0
Alexey Kohanovsky <awfj837@gmail.com>
2024-09-13 04:00:25 +0300

    refactor main.tsx to add Redux store and React Router for routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,37 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import App from './App.tsx';
+import { store } from './store';
+
+import './index.css';
+
+import DetailInfo from '@/pages/DetailInfo/';
+import Error from '@/pages/Error/';
+import Favorites from '@/pages/Favorites/';
+import Home from '@/pages/Home/';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Home />,
+		errorElement: <Error />,
+	},
+	{
+		path: '/favorites',
+		element: <Favorites />,
+	},
+	{
+		path: '/detail-info/:id',
+		element: <DetailInfo />,
+	},
+]);
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<App />
+		<Provider store={store}>
+			<RouterProvider router={router} />
+		</Provider>
 	</StrictMode>
 );
