162d59183275b7953300ce4b4ef3737fe2a8c557
Alexey Kohanovsky <awfj837@gmail.com>
2024-09-13 03:02:53 +0300

    add Search component

---
+++ src/components/Search/index.tsx
@@ -0,0 +1,54 @@
+import { useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
+import { object, string } from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+import styles from './styles.module.css';
+
+import SearchIcon from '@/assets/search.svg';
+import PageHeading from '@/components/PageHeading';
+import { debouncedSubmit } from '@/utils/form';
+
+const searchSchema = object({
+	searchTerm: string().required('Please enter a search term'),
+});
+
+export default function Search() {
+	const dispatch = useDispatch();
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		resolver: yupResolver(searchSchema),
+	});
+
+	function submitForm(data: { searchTerm: string }) {
+		debouncedSubmit(data, dispatch);
+	}
+	return (
+		<section className={styles.search}>
+			<PageHeading>
+				let's find some <span className={styles['word-emphasis']}>art</span>{' '}
+				here!
+			</PageHeading>
+
+			<form
+				onSubmit={handleSubmit(submitForm)}
+				className={styles['search-form']}
+			>
+				<input
+					className={styles['form-input']}
+					type="text"
+					{...register('searchTerm')}
+					placeholder="Search Art, Artist, Work..."
+				/>
+				<button type="submit" className={styles['form-button']}>
+					<SearchIcon />
+				</button>
+			</form>
+			<p className={styles.error}>{errors.searchTerm?.message}</p>
+		</section>
+	);
+}
