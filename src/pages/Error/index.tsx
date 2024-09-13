9bcd0d418b7565c4d023ea1149817e2848835625
Alexey Kohanovsky <awfj837@gmail.com>
2024-09-13 12:18:07 +0300

    refactor Error component to handle error object and display error message

--- src/pages/Error/index.tsx
+++ src/pages/Error/index.tsx
@@ -1,26 +1,19 @@
-import { useRouteError } from 'react-router-dom';
-
 import styles from './styles.module.css';
 
 import TextButton from '@/components/TextButton';
 import useGoBack from '@/hooks/useGoBack';
 
-type RouteError = {
-	statusText?: string;
-	message?: string;
-};
-
-export default function Error() {
-	const error = useRouteError() as RouteError;
+interface ErrorProps {
+	error?: Error;
+}
+export default function Error({ error }: ErrorProps) {
 	const goBack = useGoBack();
 
 	return (
 		<section className={styles.error}>
 			<h1>Oops!</h1>
 			<p>Sorry, an unexpected error has occurred.</p>
-			<p>
-				<i>{error.statusText || error.message}</i>
-			</p>
+			<p>{error && error?.message + '.'}</p>
 			<TextButton onClick={goBack}>Go Back</TextButton>
 		</section>
 	);
