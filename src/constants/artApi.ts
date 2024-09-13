23986b23048b5c5b28166fbd9b6ed8ede77cb769
Alexey Kohanovsky <awfj837@gmail.com>
2024-09-13 03:05:00 +0300

    add constants for artworks and artApi

---
+++ src/constants/artApi.ts
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.artsy.net/api';
+export const ITEMS_SEARCH_MAX_COUNT = 10;
---
+++ src/constants/artworks.ts
@@ -0,0 +1,8 @@
+import { ITEMS_SEARCH_MAX_COUNT } from '@/constants/artApi';
+
+export const ARTWORKS_IN_GALLERY = 12;
+export const ARTWORKS_TOTAL = 21;
+export const ARTWORKS_PER_PAGE = 3;
+
+export const artworksToSearch =
+	ITEMS_SEARCH_MAX_COUNT - (ITEMS_SEARCH_MAX_COUNT % ARTWORKS_PER_PAGE);
