4d5c5bf79f993fe7c74140d4c8746ba542ae1c7a
Alexey Kohanovsky <awfj837@gmail.com>
2024-09-18 00:57:49 +0300

    refactor: add TOTAL_PAGES constant to artworks.ts

--- src/constants/artworks.ts
+++ src/constants/artworks.ts
@@ -3,6 +3,7 @@ import { ITEMS_SEARCH_MAX_COUNT } from '@/constants/artApi';
 export const ARTWORKS_IN_GALLERY = 12;
 export const ARTWORKS_TOTAL = 21;
 export const ARTWORKS_PER_PAGE = 3;
+export const TOTAL_PAGES = 5;
 
 export const artworksToSearch =
 	ITEMS_SEARCH_MAX_COUNT - (ITEMS_SEARCH_MAX_COUNT % ARTWORKS_PER_PAGE);
