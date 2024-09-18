fb8aaa3c808ba9d3dc198501edf453429dc3d40a
Alexey Kohanovsky <awfj837@gmail.com>
2024-09-18 10:55:58 +0300

    refactor: remove unused constants and add a constant OTHER_WORKS_COUNT

--- src/constants/artworks.ts
+++ src/constants/artworks.ts
@@ -1,10 +1,4 @@
-import { ITEMS_SEARCH_MAX_COUNT } from '@/constants/artApi';
-
-export const ARTWORKS_IN_GALLERY = 12;
-export const ARTWORKS_TOTAL = 21;
 export const ARTWORKS_PER_PAGE = 3;
 export const TOTAL_PAGES = 5;
 export const INITIAL_PAGE = 1;
-
-export const artworksToSearch =
-	ITEMS_SEARCH_MAX_COUNT - (ITEMS_SEARCH_MAX_COUNT % ARTWORKS_PER_PAGE);
+export const OTHER_WORKS_COUNT = 9;
