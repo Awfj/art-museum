f793f7160e5f05df722ba256144553e9ca00cf43
Alexey Kohanovsky <awfj837@gmail.com>
2024-09-18 04:11:02 +0300

    fix: ensure gallery artworks are fetched only on initial page load

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,6 +6,7 @@ import LoadingScreen from '@/components/LoadingScreen';
 import OtherWorks from '@/components/OtherWorks';
 import Page from '@/components/Page';
 import Search from '@/components/Search';
+import { INITIAL_PAGE } from '@/constants/artworks';
 import { fetchArtworks } from '@/store/actions/artworks';
 import { Status } from '@/types/api';
 import { AppDispatch, RootState } from '@/types/store';
@@ -18,10 +19,10 @@ export default function Home() {
 	const searching = useSelector((state: RootState) => state.artworks.searching);
 
 	useEffect(() => {
-		if (artworks.length === 0) {
-			dispatch(fetchArtworks(1));
+		if (!searching && artworks.length === 0) {
+			dispatch(fetchArtworks(INITIAL_PAGE));
 		}
-	}, [dispatch, artworks]);
+	}, [dispatch, artworks, searching]);
 
 	if (status === Status.Loading && !searching && artworks.length === 0) {
 		return <LoadingScreen />;
